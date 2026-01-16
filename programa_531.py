851b6e6bffc7ad344ee608d19a2376510d3fc69a
Penpinch <brandontorresr747@gmail.com>
2026-01-16 16:49:37 -0600

    restructuring

--- programa_531.py
+++ programa_531.py
@@ -1,129 +1,88 @@
-from abc import ABC, abstractmethod
-
 #Devuelve una lista con los porcentajes a usar en cada semana --> [[], [],[]]
-class CrearListaPorcentajes:
+class MakePorcentagesList:
     def __init__(self):
-        self.porcentajes_todos = [0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95]
-        self.porcentajes_ordenados = []
+        self.all_porcentages = [0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95]
+        self.sorted_porcentages = []
 
-    def porcentajes_lista(self):
-        for i in range(1, 4):
-            if i == 1:
-                agregar = self.porcentajes_todos[0:5:2]
-                self.porcentajes_ordenados.append(agregar)
+    def porcentagesList(self):
+        for i in range(3):
+            if i == 0:
+                agregar = self.all_porcentages[0:5:2]
+                self.sorted_porcentages.append(agregar)
                 
-            elif i == 2:
-                agregar = self.porcentajes_todos[1::2]
-                self.porcentajes_ordenados.append(agregar)
-                
+            elif i == 1:
+                agregar = self.all_porcentages[1::2]
+                self.sorted_porcentages.append(agregar)
             else:
-                agregar = self.porcentajes_todos[2::2]
-                self.porcentajes_ordenados.append(agregar)
-        return self.porcentajes_ordenados
-  
-class Estructura(ABC):#nomas para usar lo de metodos abstractos ¯\_(ツ)_/¯
-    def __init__(self, porcentajes, tm, unidad):
-        self.porcentajes = porcentajes
+                agregar = self.all_porcentages[2::2]
+                self.sorted_porcentages.append(agregar)
+        return self.sorted_porcentages
+
+class Structure:
+    def __init__(self, porcentages, tm):
+        self.porcentages = porcentages
         self.tm = tm
-        self.unidad = unidad
-        
-    @abstractmethod
-    def calcular_pesos_semana_1(self):
-        porcentajes = self.porcentajes[0]
-        for porcentaje in porcentajes:
-            peso = self.tm * porcentaje
-            if self.unidad == "lbs":
-                peso = (self.tm * porcentaje) * 2.20462
-                print(f"{round(porcentaje * 100)}%: {round((peso), 2)} lbs.")
-            else:
-                print(f"{round(porcentaje * 100)}%: {round((peso), 2)} kg.")
-    
-    @abstractmethod
-    def calcular_pesos_semana_2(self):
-        porcentajes = self.porcentajes[1]
-        for porcentaje in porcentajes:
-            if self.unidad == "lbs":
-                peso = (self.tm * porcentaje) * 2.20462
-                print(f"{round(porcentaje * 100)}%: {round((peso), 2)} lbs.")
-            else:
-                print(f"{round(porcentaje * 100)}%: {round((self.tm * porcentaje), 2)} kg.")
-    
-    @abstractmethod
-    def calcular_pesos_semana_3(self):
-        porcentajes = self.porcentajes[2]
-        for porcentaje in porcentajes:
-            if self.unidad == "lbs":
-                peso = (self.tm * porcentaje) * 2.20462
-                print(f"{round(porcentaje * 100)}%: {round((peso), 2)} lbs.")
-            else:
-                print(f"{round(porcentaje * 100)}%: {round((self.tm * porcentaje), 2)} kg.")
-    
-    @abstractmethod
-    def calcular_pesos_semana_deload(self):
-        if self.unidad == "lbs":
-            print(f"3 series con 50-60%, es decir: {round((self.tm*2.20462)/2)}-{round((self.tm*2.20462)*0.6)} lbs.")
-        else:
-            print(f"3 series con 50-60%, es decir: {round(self.tm/2)}-{round(self.tm*0.6)} kg.")
-    
-class Ejercicio(Estructura):
-    def calcular_pesos_semana_1(self):
-        return super().calcular_pesos_semana_1()
-        
-    def calcular_pesos_semana_2(self):
-        return super().calcular_pesos_semana_2()
-        
-    def calcular_pesos_semana_3(self):
-        return super().calcular_pesos_semana_3()
-        
-    def calcular_pesos_semana_deload(self):
-        return super().calcular_pesos_semana_deload()
-    
-# Trabajar todo en un mismo sistema de medida
-def conversion_unidades(unorm, unidad):
-    unidad = unidad.lower()
-    if unidad == "kg":
-        return unorm
-    elif unidad == "lbs":  
-        unorm = unorm/2.20462
-        return unorm    
- 
+        self.lift_weights_list = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+    def calculateWeights(self):
+        for week_index in range(len(self.porcentages)):
+            for porcentage_index, porcentage in enumerate(self.porcentages[week_index]):
+                self.lift_weights_list[week_index][porcentage_index] = self.tm * porcentage
+        return self.lift_weights_list
+
+    def deloadWeek(self):
+        return self.tm * 0.5, self.tm *0.6
+
+# Trabajar todo en Kg
+def measurementUnit(weight, unit: str):
+    unit = unit.lower()
+    if unit == "kg":
+        return weight
+    elif unit == "lbs":  
+        weight = weight / 2.20462
+        return weight 
+
 #no cambian
-porcentaje = CrearListaPorcentajes()
-porcentajes = porcentaje.porcentajes_lista()        
+porcentages = MakePorcentagesList().porcentagesList()
+def getData():
+    print("Recomendado en ejercicios compuestos como: Press de banca, Sentadilla y Peso Muerto.\n")
+    verification = str(input("¿Conoce su máximo de trabajo(TM)?: ")).lower()
 
-print("Recomendado en ejercicios compuestos como: Press de banca, Sentadilla y Peso Muerto.\n")
+    while verification != "si" and verification != "no":
+        print("No existe esta opción.")
+        verification = str(input("¿Conoce su máximo de trabajo(TM)?: ")).lower()
 
-verificacion = str(input("¿Conoce su máximo de trabajo(TM)?: ")).lower()
-
-while verificacion != "si" and verificacion != "no":
-    print("No existe esta opción.")
-    verificacion = str(input("¿Conoce su máximo de trabajo(TM)?: ")).lower()
-
-if verificacion == "si":
-    tm = float(input("Máximo de trabajo: "))
-    unidad = input("kg/lbs: ")
-    tm = conversion_unidades(tm, unidad)
-    
-else:
-    unorm = float(input("1RM real: "))    
-    unidad = input("kg/lbs: ")
-    tm = conversion_unidades(unorm, unidad) * 0.90
-   
-ejercicio = Ejercicio(porcentajes, tm, unidad)
-
-def mostrar_plan(ejercicio, tm, unidad):
-    if unidad == "lbs":
-        print(f"\nTM: {tm*2.20462}")
+    if verification == "si":
+        tm = float(input("Máximo de trabajo: "))
+        unit = input("kg/lbs: ")
+        tm = measurementUnit(tm, unit)
     else:
-        print(f"\nTM: {tm}")
-        
-    print("\nSemana 1 ")
-    ejercicio.calcular_pesos_semana_1()
-    print("\nSemana 2")
-    ejercicio.calcular_pesos_semana_2()
-    print("\nSemana 3")
-    ejercicio.calcular_pesos_semana_3()
-    print("\nSemana 4")
-    ejercicio.calcular_pesos_semana_deload()
+        one_RM = float(input("1RM real: "))    
+        unit = input("kg/lbs: ")
+        one_RM = measurementUnit(one_RM, unit)
+        tm = one_RM * 0.90
+
+    return tm, unit
+tm, unit = getData()
+excercise = Structure(porcentages, tm)
+
+def show_plan(excercise: Structure, tm, unit, porcentages: MakePorcentagesList):  
+    display_unit = "kg" if unit == "kg" else "lbs"
+    conv = 1.0 if unit == "kg" else 2.20462
+    print(f"\nTM: {round(tm * conv, 2)} {display_unit}.\n")
+
+    weights = excercise.calculateWeights()
+    excercise_deload = excercise.deloadWeek()
     
-mostrar_plan(ejercicio, tm, unidad)
+    c = 1
+    for week_index in range(len(weights)):
+        print(f"Week {c}:")
+        c = c + 1 
+        for weight_index, weight in enumerate(weights[week_index]): 
+            display_weight = round(weight * conv, 2)
+            print(f"{porcentages[week_index][weight_index] * 100:.0f}% : {display_weight} {display_unit}")
+
+    print("\nDeload week:")
+    print(f"3x50-60%: {round((excercise_deload[0] * conv), 2)} - {round((excercise_deload[1] * conv), 2)} {display_unit}")
+
+show_plan(excercise, tm, unit, porcentages)
